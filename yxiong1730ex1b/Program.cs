e9b8e8c680902e079b3b80e883cf377522aa5dd5
Ya Houa Xiong <UN6744UO@go.minnstate.edu>
2019-09-06 02:32:20 -0500

    Completed Ex1b

--- yxiong1730ex1b/Program.cs
+++ yxiong1730ex1b/Program.cs
@@ -16,7 +16,7 @@ namespace yxiong1730ex1b
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmInvoiceTotal());
         }
     }
 }
---
+++ yxiong1730ex1b/frmInvoiceTotal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yxiong1730ex1b
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            txtDiscountAmount.Text = 
+                (Convert.ToDecimal(txtSubtotal.Text) 
+                * Convert.ToDecimal(txtDiscountPercent.Text) / 100).ToString("0.00");
+            txtTotal.Text = (Convert.ToDecimal(txtSubtotal.Text)
+                - Convert.ToDecimal(txtDiscountAmount.Text)).ToString("0.00");
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
